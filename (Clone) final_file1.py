d2935433efbf1654dc15f7b7cf8afc7ab00a3b20
srishty-07 <srishty-07>
2023-09-27 06:30:21 +0000

    new_file(link)

---
+++ (Clone) final_file1.py
@@ -0,0 +1,28 @@
+# Databricks notebook source
+df=spark.read.json("dbfs:/mnt/saunextadls/raw/json/")
+
+# COMMAND ----------
+
+from pyspark.sql.functions import *
+
+# COMMAND ----------
+
+df1=df.withColumn("ingestiondate",current_timestamp()).withColumn("path",input_file_name())
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC create schema if not exists json
+
+# COMMAND ----------
+
+df1.write.mode("overwrite").option("overwriteSchema", "true").option("path","dbfs:/mnt/saunextadls/raw/output/srishty/json").saveAsTable("json.bronze")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from json.bronze
+
+# COMMAND ----------
+
+
